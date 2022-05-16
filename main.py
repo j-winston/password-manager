10901428edfba55660c83947874fcd1b56457878
James Winston <kenjameswinston@gmail.com>
2022-05-16 10:44:27 -0400

    Add functionality to read/write JSON files

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from tkinter import messagebox
 from tabulate import tabulate
 import random
 import pyperclip
-
+import json
+import time
 # Window dimension constants
 WINDOW_HEIGHT = 200
 WINDOW_WIDTH = 200
@@ -51,32 +52,70 @@ def save_data():
     email = email_str.get()
     password = password_str.get()
 
-    # Dictionary to hold user's entry
-    user_df = {"Website": [website], "Email/Username": [email], "Password": [password]}
+    # convert website to lower case
+    website = website.lower()
+
+    # JSON dictionary
+    new_data = {
+        website: {
+            "email": email,
+            "password": password
+        }
+    }
 
     # Give warning if there are empty fields
-    if len(website) == 0 or len(email) == 0 or len(password) == 0:
+    if len(website) == 0 or len(password) == 0:
         messagebox.showwarning(title="Uh oh!", message="Please don't leave empty fields")
     # Otherwise, ask for confirmation and write to file
     else:
-        # Ask for confirmation
-        entry_confirm = messagebox.askokcancel(title=website, message=f"Website: {website}\nEmail: {email}\nIs this correct?")
-        if entry_confirm:
-            # Write data to file
-            with open("data.txt", "a+") as f:
-                table = tabulate(user_df, tablefmt="plain")
-                # If there's data in the file, don't re-write the header!
-                f.seek(0)
-                data = f.read(100)
-                if len(data) > 0:
-                    f.write("\n")
-                    f.write(table)
-                else:
-                    table = tabulate(user_df, headers=["Website", "Email/Username", "Password"])
-                    f.write(table)
-            # Clear all entry fields
-            website_entry.delete(0, END)
-            password_entry.delete(0, END)
+        try:
+            # Look for "data.json"
+            with open("data.json", "r") as file:
+                data = json.load(file)
+                data.update(new_data)
+        # If it doesn't exist, create it and put something in it
+        except FileNotFoundError:
+            with open("data.json", "w") as file:
+                json.dump(new_data, file)
+        else:
+            # If file is opened and loaded, we can write new data to it
+            with open("data.json", "w") as file:
+                json.dump(data, file, indent=4)
+
+        # Clear all entry fields
+        website_entry.delete(0, END)
+        password_entry.delete(0, END)
+
+
+def search():
+    website_text = website_str.get()
+
+    # convert to lower case
+    website_text = website_text.lower()
+
+    # try to open and find key
+    try:
+        with open("data.json", "r") as file:
+            file_data = json.load(file)
+            email = file_data[website_text]["email"]
+            password = file_data[website_text]["password"]
+    # if key doesn't exist of if there's no json file, throw an error
+    except FileNotFoundError:
+        messagebox.showerror(title="No json file", message="No json file exists. Please create an entry, first")
+    except KeyError:
+        messagebox.showerror(title="Entry not found", message=f"An entry for {website_text} does not exist.")
+    else:
+        # display record
+        email_entry.delete(0, END)
+        password_entry.insert(0, password)
+        email_entry.insert(0, email)
+
+        # confirm clipboard copied
+        clipboard_message = mesg_canvas.create_text(110, 30, text="Copied to clipboard!")
+        mesg_canvas.after(800, mesg_canvas.delete, clipboard_message)
+
+
+
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -92,6 +131,10 @@ canvas = Canvas(root, height=WINDOW_HEIGHT, width=WINDOW_WIDTH, bg=BACKGROUND_CO
 lock_logo = PhotoImage(file="logo.png")
 lock_image = canvas.create_image(100, 100, image=lock_logo)
 canvas.grid(row=0, column=1)
+
+# Message canvas
+mesg_canvas = Canvas(root, height=50, width=200, bg=BACKGROUND_COLOR, highlightthickness=0)
+mesg_canvas.grid(row=5, column=1)
 
 # Labels
 website_label = Label(text="Website:", bg=BACKGROUND_COLOR)
@@ -112,22 +155,23 @@ website_entry = Entry(root, textvariable=website_str, width=35)
 website_entry.grid(row=1, column=1, columnspan=2)
 website_entry.focus()
 
-email_entry = Entry(root, textvariable=email_str, width=35)
-email_entry.grid(row=2, column=1, columnspan=2)
+email_entry = Entry(root, textvariable=email_str, width=50)
+email_entry.grid(row=2, column=1, columnspan=3)
 email_entry.insert(0, "kenjameswinston@gmail.com")
 
-password_entry = Entry(root, textvariable=password_str, width=21)
-password_entry.grid(row=3, column=1)
+password_entry = Entry(root, textvariable=password_str, width=35)
+password_entry.grid(row=3, column=1,columnspan=2)
 
 # Buttons
-
 # The GENERATE button
 generate_button = Button(root, text="Generate", bg=BACKGROUND_COLOR, width=11, command=generate_password)
-generate_button.grid(row=3, column=2)
+generate_button.grid(row=3, column=3)
 
 # The ADD button
 add_button = Button(root, text="Add", bg=BACKGROUND_COLOR, width=33, command=save_data)
 add_button.grid(row=4, column=1, columnspan=2)
 
-
+# The search button
+search_button = Button(root, text="Search", bg=BACKGROUND_COLOR, width=11, command=search)
+search_button.grid(row=1, column=3)
 root.mainloop()
